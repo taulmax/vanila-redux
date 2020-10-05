647ebe8896d359ad1f5dee06381b538bbdd5869e
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-10-06 02:43:22 +0900

    Redux Reducer 연습
    Redux Reducer 연습

--- vanila-redux/src/index.js
+++ vanila-redux/src/index.js
@@ -1,24 +1,30 @@
+import { createStore } from "redux";
+
 const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-
-number.innerText = count;
-
-const updateCount = () => {
-  number.innerText = count;
+const countModifier = (count = 0, action) => {
+  if (action.type === "Plus") {
+    return count + 1;
+  } else if (action.type === "Minus") {
+    return count - 1;
+  } else {
+    return count;
+  }
 }
 
-const handlePlus = () => {
-  count = count + 1;
-  updateCount();
-}
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  count = count - 1;
-  updateCount();
-}
+countStore.dispatch({ type: "Plus" });
+countStore.dispatch({ type: "Plus" });
+countStore.dispatch({ type: "Plus" });
+countStore.dispatch({ type: "Plus" });
+countStore.dispatch({ type: "Minus" });
+countStore.dispatch({ type: "Minus" });
+countStore.dispatch({ type: "Minus" });
+countStore.dispatch({ type: "Minus" });
+countStore.dispatch({ type: "Minus" });
+countStore.dispatch({ type: "Minus" });
 
-plus.addEventListener("click", handlePlus);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
