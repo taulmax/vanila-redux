da2cd5cc81feca4a3a963f9a4e3d7281390a5d82
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-10-07 14:02:23 +0900

    ToDoApp Complete
    ToDoApp Complete

--- vanila-redux/src/index.js
+++ vanila-redux/src/index.js
@@ -52,13 +52,26 @@ const ul = document.querySelector(".toDoUl");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+const addTask = (task) => {
+  return {
+    type: ADD_TODO,
+    task
+  }
+};
+
+const deleteTask = (id) => {
+  return {
+    type: DELETE_TODO,
+    id
+  }
+};
+
 const toDoReducer = (state = [], action) => {
-  console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return state;
+      return [...state, { task: action.task, id: Date.now() }];
     case DELETE_TODO:
-      return state;
+      return state.filter(task => task.id !== action.id);
     default:
       return state;
   }
@@ -66,11 +79,40 @@ const toDoReducer = (state = [], action) => {
 
 const toDoStore = createStore(toDoReducer);
 
+toDoStore.subscribe(() => console.log(toDoStore.getState()));
+
+const dispatchAddTask = (task) => {
+  toDoStore.dispatch(addTask(task));
+}
+
+const dispatchDeleteTask = (e) => {
+  const id = parseInt(e.target.parentNode.id);
+  toDoStore.dispatch(deleteTask(id));
+}
+
+const paintTasks = () => {
+  const tasks = toDoStore.getState();
+  ul.innerText = "";
+  tasks.forEach(task => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    li.id = task.id;
+    li.innerText = task.task;
+    btn.innerText = "❌";
+    li.appendChild(btn);
+    btn.addEventListener("click", dispatchDeleteTask);
+    ul.appendChild(li);
+  });
+}
+
+toDoStore.subscribe(paintTasks);
+
+
 const onSubmit = e => {
   e.preventDefault();
   const task = input.value;
   input.value = "";
-  toDoStore.dispatch({ type: ADD_TODO, text: task });
+  dispatchAddTask(task);
 }
 
 form.addEventListener("submit", onSubmit);
